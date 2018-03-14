1ad35b5edb915850a92716dc676e7bea402b23f5
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-14 12:20:58 -0700

    use list comprehension

--- leet.py
+++ leet.py
@@ -16,19 +16,27 @@
 #     return "".join(characters)
 
 
+# def translate_leet(message):
+#     leet = {
+#         'a': '@',
+#         'o': '0',
+#         'e': '3',
+#         'l': '1',
+#         's': '5',
+#         't': '7'
+#     }
+#     letters = list(message)
+#     for i in range(len(letters)):
+#         if letters[i].lower() in leet:
+#             letters[i] = leet[letters[i].lower()]
+#     return "".join(letters)
+
+
 def translate_leet(message):
-    leet = {
-        'a': '@',
-        'o': '0',
-        'e': '3',
-        'l': '1',
-        's': '5',
-        't': '7'
-    }
-    letters = list(message)
-    for i in range(len(letters)):
-        if letters[i].lower() in leet:
-            letters[i] = leet[letters[i].lower()]
+    leet = {'a': '@', 'o': '0', 'e': '3',
+            'l': '1', 's': '5', 't': '7'}
+
+    letters = [leet.get(letter.lower(), letter) for letter in message]
     return "".join(letters)
 
 print translate_leet("Estrella")
